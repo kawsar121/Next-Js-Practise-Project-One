e1fd010e4034493f061c3f8680e0edfbc82d9c02
kawsar121 <tohidulislamkawsarbhuiyan@gmail.com>
2025-12-14 20:42:32 +0600

    setup file stracture

---
+++ src/app/(Pages)/about/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>This is About Page</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure quisquam quae repudiandae consequuntur sint ut id praesentium similique eveniet harum a, ullam molestias aperiam voluptatibus, perferendis, cupiditate eos autem hic!</p>
+        </div>
+    );
+};
+
+export default AboutPage;
